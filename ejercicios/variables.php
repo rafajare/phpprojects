4c8c73b60aa057b1c604ee876d24c6a06b294d33
unknown <rafa_8_6_96@hotmail.com>
2015-09-28 13:10:37 +0200

    Iniciando git

---
+++ ejercicios/variables.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Control de errores</title>
+</head>
+<body>
+	<?php
+			//Definición de variable con php
+			$frase="Paco es un campeón";
+			echo $frase;
+
+			echo"<br>";
+			$numero=13;
+			echo $numero;
+
+			echo "<br>";
+			// Numero decimal
+			$numerodecimal=1.445;
+			echo $numerodecimal;
+
+			echo"<br>";
+			$condicion=true;
+			echo $condicion;
+			echo"<br>";
+
+			echo"Esto es una forma de imprimir un String";
+			$frase2="<br>"."El valor de la variable es $numero";
+			echo $frase2;
+
+	?>
+</body>
+</html>
---
+++ holamundo/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Mi primer PHP</title>
+</head>
+<body>
+	Esto es HTML
+	<br>
+	<?php
+		echo"Hola mundo, esto es PHP"; //Comentario de una sola línea
+		echo "<br>"; //Introducir salto de línea con código html, usando el método php
+		/*
+		Comentario de varias lineas
+		*/
+		echo "Esto abarca
+				multiples l&iacuteneas. Los saltos de línea también 
+					se mostrarán";
+					
+	?>
+</body>
+</html>
