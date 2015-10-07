1565492a928a415942327198e57b97fc7e95ea8a
rafapicazo <rafa_8_6_96@hotmail.com>
2015-10-07 12:37:34 +0000

    actualizacion menu multilenguaje

--- practicas/multilenguaje_menu.php
+++ practicas/multilenguaje_menu.php
@@ -1,42 +1,44 @@
 <?php
-//Array multilenguaje
 $lang="sp";
 $menu=array(
-	"titulo"=>array(
-		"sp"=>"Math Dice",
-		"en"=>"Math dice"
-	    ),
-	"portada"=>array(
-		"sp"=>"Inicio",
-		"en"=>"Home"
-		),
-	"instrucciones"=>array(
-		"sp"=>"Instrucciones",
-		"en"=>"Instructions"
-		),
-	"tiposJuego"=>array(
-		"sp"=>"Tipos juego",
-		"en"=>"Game choice",
-		"submenu"=>array(
-			"tipo1"=>array(
-				"sp"=>"Juego tradicional",
-				"en"=>"Traditional game",
-			),
-			"tipo2"=>array(
-				"sp"=>"Juego modificado",
-				"en"=>"Game modified",
-				),
-			"tipo3"=>array(
-				"sp"=>"Juego niños",
-				"en"=>"Kid game",
-				)
-			)
-		)
-	)
+    "titulo"=>array(
+      "sp"=>"Math Dice",
+      "en"=>"Math Dice"
+    ),
+    "portada"=>array(
+      "sp"=>"Inicio",
+      "en"=>"Home"
+    ),
+    "instrucciones"=>array(
+      "sp"=>"Instrucciones",
+      "en"=>"Instructions"
+    ),
+    "tiposJuego"=>array(
+      "sp"=>"Tipos juego",
+      "en"=>"Game choice",
+      "submenu"=>array(
+          "tipo1"=>array(
+             "sp"=>"Juego Tradicional",
+             "en"=>"Traditional game",
+           ),
+          "tipo2"=>array(
+             "sp"=>"Juego modificado",
+             "en"=>"Game modified",
+           ),
+          "tipo3"=>array(
+             "sp"=>"Juego niños",
+             "en"=>"Kid game",
+           )
+           
+        )
+    
+    
+    )
+    
+  )
+?>
 
-?>		
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
   <title>Bootstrap Case</title>
   <meta charset="utf-8">
@@ -46,65 +48,41 @@ $menu=array(
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 <body>
-    
 
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <!--Llamamos al array menu, y buscará la clave titulo. Y llamamos al array lenguaje-->
+      <a class="navbar-brand" href="#"><?php echo $menu['titulo'][$lang]?></a>
     </div>
     <div>
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <!--Llamamos al array menu, y buscará la clave portada(que pondrá 'Inicio' en la barra de menu). Y llamamos al array lenguaje-->
+        <li class="active"><a href="#"><?php echo $menu['portada'][$lang]?></a></li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $menu['tiposJuego'][$lang]?><span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Page 1-1</a></li>
-            <li><a href="#">Page 1-2</a></li>
-            <li><a href="#">Page 1-3</a></li>
-          </ul>
+                    <?php 
+                    foreach( $menu['tiposJuego'] as $clave => $valor ){
+                      // Almacenamos las claves del contenido del array en $clave y hacemos un if
+                      if($clave == 'submenu'){
+                          //Si el array clave coincide con lo del submenu procederá al siguiente foreach
+                          // Aqui el valor seria el array submenu que ha encontrado antes ?..
+                          // Imprimira los valores del array.
+                          //Vuelca el valor de submenu en una sentencia foreach. De esta forma hará submenu 1, submenu2, submenu3
+                        foreach ($valor as $games){ ?>
+                            <li><a href="#"><?php echo $games[$lang] ?></a></li>
+                    <?php } 
+                         }
+                    } ?>
+                  </ul>
         </li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
+        <li><a href="#"><?php echo $menu['instrucciones'][$lang]?></a></li>
+        <li><a href="#"><?php echo $menu['acercaDe'][$lang]?></a></li>
       </ul>
     </div>
   </div>
 </nav>
-  
-<div class="container">
-  <div class="jumbotron">
-    <h1>My first Bootstrap website!</h1>      
-    <p>This page will grow as we add more and more components from Bootstrap...</p>      
-    <a href="#" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span> Search</a>
-  </div>
 
-  <div class="row">
-    <div class="col-md-3">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </div>
-    <div class="col-md-3"> 
-      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    </div>
-    <div class="col-md-3"> 
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-    </div>
-    <div class="col-md-3">
-      <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Menu 1 <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Submenu 1-1</a></li>
-            <li><a href="#">Submenu 1-2</a></li>
-            <li><a href="#">Submenu 1-3</a></li>                        
-          </ul>
-        </li>
-        <li><a href="#">Menu 2</a></li>
-        <li><a href="#">Menu 3</a></li>
-      </ul>
-    </div>
-    <div class="clearfix visible-lg"></div>
-  </div>
-</div>
 
 </body>
 </html>
