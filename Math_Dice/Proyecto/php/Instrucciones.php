d0032f01e08a29a7c915e7ed1bdc58dc9bc3f305
rafapicazo <rafa_8_6_96@hotmail.com>
2015-11-25 23:24:54 +0000

    .

--- Math_Dice/Proyecto/php/Instrucciones.php
+++ Math_Dice/Proyecto/php/Instrucciones.php
@@ -3,13 +3,17 @@ include "configuracion.php";
 ?>
 <html>
   <head>
-    <title>Bootstrap Case</title>
+    <title>Instrucciones</title>
+    <!--No coge esto--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <!--No coge esto--><meta charset="UTF-8">
+    <!--No coge esto--><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../css/Instrucciones.css" type="text/css" />
-    <!--No coge esto<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>-->
+    
+    
  </head>
  <body>
      
--- Math_Dice/Proyecto/php/cabecera_junior.php
+++ Math_Dice/Proyecto/php/cabecera_junior.php
@@ -10,8 +10,10 @@
 
 <html>
   <head>
-    <title>Bootstrap Case</title>
-    <meta charset="utf-8">
+    <title>MATH DICE</title>
+    <!--No coge esto--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <!--No coge esto--><meta charset="UTF-8">
+    <!--No coge esto--><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
